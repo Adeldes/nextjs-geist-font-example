e834c02f4615ce96c02e581ed76f776f8d59fcab
Adel Shikh <adeldes@gmail.com>
2025-09-04 12:16:39 +0300

    Add .next/static/chunks/src_app_page_tsx_f3e6a983._.js 1756977397794

---
+++ .next/static/chunks/src_app_page_tsx_f3e6a983._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ed13eca1._.js",
+  "static/chunks/node_modules_a7cb6c78._.js"
+],
+    source: "dynamic"
+});
